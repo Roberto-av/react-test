ac277d20dd026ce6e4e64ddac5922d7f8a601d9c
Roberto Aviles <avilesrobertoe@gmail.com>
2024-02-23 15:53:38 -0700

    Detalles

--- src/Search.js
+++ src/Search.js
@@ -5,7 +5,7 @@ function Search() {
   return (
     <Form inline className="w-100 mb-3">
     <div className="d-flex flex-fill">
-      <FormControl type="text" placeholder="Buscar" className="flex-grow-1 mr-2" />
+      <FormControl type="text" placeholder="Buscar Pokemon" className="flex-grow-1 mr-2" />
       <Button variant="primary" style={{ marginLeft: '5px' }}>Buscar</Button>
     </div>
   </Form>
